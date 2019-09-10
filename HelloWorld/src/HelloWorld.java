0e45e1de885caf9cbfb867876d3ba66a92fa483c
unknown <alexander.mompoint@gmail.com>
2019-09-10 18:58:39 -0400

    First Commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[] args)
+	{
+		// This comment is to make it so that I hope this is different
+		System.out.println("Hello, World!;");
+	}
+}
